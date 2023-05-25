706f9d84c88d302b102633e789b6b131ab77a492
lokurapc <lokurapc-web@yahoo.com.ar>
2023-05-25 10:25:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myMenu() {
+	var x = document.getElementById("menu");
+		x.classList.toggle("chico");
+	var x = document.getElementById("top");
+		x.classList.toggle("chico");
+	var x = document.getElementById("content");
+		x.classList.toggle("chico");
+}
